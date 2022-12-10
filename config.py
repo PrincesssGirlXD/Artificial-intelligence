3f3818395df2db192c966154ed4fde8e36fb40ae
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-10 20:09:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-BOT_ID = getenv("BOT_ID", "")#token first 10 digits
+BOT_TOKEN = getenv("BOT_TOKEN", "5906050479:AAHydpul_rBm5srQI4kIWIJUdSqjmQGiNS0")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://rick:kurumi@cluster0.kvvnh4v.mongodb.net/?retryWrites=true&w=majority")
+BOT_ID = getenv("BOT_ID", 5906050479)#token first 10 digits
 AI_API_KEY = getenv("AI_API_KEY", "")
 AI_ID = int(getenv("AI_ID", ""))
